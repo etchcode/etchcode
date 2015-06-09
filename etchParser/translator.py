8a81f5f13d727de69153383a81af63cf2c838308
Daniel F <df@openaliasbox.org>
2015-06-08 20:40:09 -0700

    fix error with translating empty file

--- etchParser/translator.py
+++ etchParser/translator.py
@@ -3,7 +3,7 @@ from xml_creator import xmlcreator
 def translate(string):
     print "input"
     if string.replace(" ", "") == "":
-        return '''<script x="0" y="0"></scripts>'''
+        return "<scripts></scripts>"
     print string
     main = xmlcreator()
     return main.translates(string)
