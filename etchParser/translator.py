722126a6e179d48b2b6c4ef13f6d2f00b6745e11
Sam Schickler <gadgetsam@gmail.com>
2015-06-08 17:52:52 -0700

    added script tag on output

--- etchParser/translator.py
+++ etchParser/translator.py
@@ -3,7 +3,7 @@ from xml_creator import xmlcreator
 def translate(string):
     print "input"
     if string.replace(" ", "") == "":
-        return ""
+        return '''<script x="0" y="0"></scripts>'''
     print string
     main = xmlcreator()
     return main.translates(string)
