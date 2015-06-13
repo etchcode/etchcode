9a9ded60f3c1baa03ed322903fa2bacb70fe8ff4
Daniel F <df@openaliasbox.org>
2015-06-12 18:24:23 -0700

    docs changes

--- static/docs/scripts/main.js
+++ static/docs/scripts/main.js
@@ -4,6 +4,20 @@
 	
 	angular.module("docs", [])
 	
+	.filter("title", function() {
+		return function(input) {
+			return input[0].toUpperCase() + input.substr(1, input.length);
+		};
+	})
+	
+	.directive("generalDocumentation", function() {
+		return {
+			restrict: "E",
+			replace: true,
+			templateUrl: "templates/general_documentation.html"
+		};
+	})
+			
 	.controller("docsController", ["$scope", "$http", function($scope, $http) {
 		$http.get("docs.json").then(function(data){
 			var docsData = data.data;
@@ -23,6 +37,8 @@
 				
 				return {"name": name, "property": property[name]};
 			}
-		}
+		};
+		
+		$scope.describe = "general"; // this is what is shown in the main view
 	}]);
 }());
