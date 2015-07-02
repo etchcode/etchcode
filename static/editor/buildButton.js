132a223dfa0fc9fe88776cba71e97f80a5253a2e
Sam Schickler <gadgetsam@gmail.com>
2015-07-01 17:04:38 -0700

    Revert 787aff4..b9568c0
    This rolls back to commit 787aff48dd1cabe67a55017c7ae0f897020766ea.

---
+++ static/editor/buildButton.js
@@ -0,0 +1,18 @@
+/* globals angular */
+
+(function(){
+	angular.module("buildButton", ["sprites", "render"])
+	
+	.controller("buildButtonController", ["$scope", "renderService", "spriteData", function($scope, render, spriteData){
+		this.dropdown = false;
+		
+		this.render = function(){
+			render.project(spriteData.list).then(function(response){
+				$scope.$apply(function(){
+					angular.element("run-project").scope().run(response);
+					angular.element("run-project").scope().show = true;
+				});
+			});
+		};
+	}]);
+}());
---
+++ static/editor/main.js
@@ -0,0 +1,7 @@
+/* globals angular */
+(function(){
+	"use strict";
+	
+	angular.module("webapp", ["ngSanitize", "ui.codemirror", "sprites", "editor", "editableImage", "buildButton", "render", "colorInput"]);
+	
+}());
