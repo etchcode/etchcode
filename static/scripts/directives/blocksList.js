734e9592acb7a0e6206bfb229a67dbec137a08bb
Daniel F <danielf@openmailbox.org>
2015-09-07 13:54:29 -0700

    basic blocks pane
    No dragging blocks in or blocks icons

--- static/scripts/directives/blocksList.js
+++ static/scripts/directives/blocksList.js
@@ -10,17 +10,12 @@
                 templateUrl: "partials/blocksList.html",
                 controller: ["$scope", "$http", function($scope, $http){
                     $scope.blocks = {};
-                    $scope.scratchParentOrder = [ // the order of the categories in scratch. Goes across row by row
-                        "Motion",
-                        "Events",
-                        "Looks",
-                        "Control",
-                        "Sound",
-                        "Sensing",
-                        "Pen",
-                        "Operators",
-                        "Data"
-                    ]
+                    $scope.scratchParentOrder = ["motion", "events", "looks", "control", "sound", "sensing", "pen", "operators", "data"];// the order of the categories in scratch. Goes across row by row
+                    $scope.blocksListParent = $scope.scratchParentOrder[0]; // the parent currently clicked on
+
+                    $scope.setBlocksListParent = function(newParent){
+                        $scope.blocksListParent = newParent;
+                    };
 
                     $http.get("/api/blocks.json").then(function(response){
                         if(response.status === 200){
