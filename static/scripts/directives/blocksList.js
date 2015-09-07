3e6b62c28b0f9a8f2b42b41b0c5b6922a7525eee
Daniel F <danielf@openmailbox.org>
2015-09-07 15:04:40 -0700

    more changes to blocksList

--- static/scripts/directives/blocksList.js
+++ static/scripts/directives/blocksList.js
@@ -10,16 +10,15 @@
                 templateUrl: "partials/blocksList.html",
                 controller: ["$scope", "$http", function($scope, $http){
                     $scope.blocks = {};
-                    $scope.scratchParentOrder = ["motion", "events", "looks", "control", "sound", "sensing", "pen", "operators", "data"];// the order of the categories in scratch. Goes across row by row
-                    $scope.blocksListParent = $scope.scratchParentOrder[0]; // the parent currently clicked on
+                    $scope.blocksListParent = "motion"; // the parent currently clicked on
 
                     $scope.setBlocksListParent = function(newParent){
                         $scope.blocksListParent = newParent;
                     };
 
-                    $http.get("/api/blocks.json").then(function(response){
+                    $http.get("pages/docs/data/docs.json").then(function(response){
                         if(response.status === 200){
-                            $scope.blocks = response.data.snapNames;
+                            $scope.blocks = response.data.etchNames;
                         }
                         else{
                             throw new Error("Failed to load blocks.json");
