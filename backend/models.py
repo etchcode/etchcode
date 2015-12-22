706ede3475029e23dc6e5cac6fdcbb94d724142e
Daniel F <danielf@openmailbox.org>
2015-12-22 11:46:25 -0500

    by default make users active

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):  # a user of etch
 	email = ndb.StringProperty() # login indentifier
-	active = ndb.BooleanProperty() # are they allowed to log in?
+	active = ndb.BooleanProperty(default=True) # are they allowed to log in?
 
 	# profile
 	username = ndb.StringProperty()
