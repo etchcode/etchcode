7c2f63ae6538ebff728ebeb2cf941f8abba4caa8
Daniel F <danielf@openmailbox.org>
2015-12-28 22:41:25 -0500

    rewrite parser with indented blocks w/o comments

--- backend/etchParser/transformer.py
+++ backend/etchParser/transformer.py
@@ -1,81 +1,55 @@
-# flake8: noqa # Sam: This line is for something in my editor
-from pyparsing import *
+from pyparsing import Word, OneOrMore, alphanums, nums, QuotedString,\
+    Suppress, Forward, oneOf, Group, Optional, indentedBlock, ZeroOrMore,\
+    stringEnd
+import blocks
 
-# class printx:
-#      def __init__(self, x):
-#          self.x = x
-#      def __repr__(self):
-#          print slef
-class transformList:
 
-    def __init__(self, string):
-        self.string = string
+class toList:
+    def __init__(self, etch_code_string):
+        self.etch_code_string = etch_code_string
+
+        indentationStack = [1]
+
+        # basic building blocks
+        keyword = Word(alphanums)
+        integer = Word(nums).setParseAction(lambda i: int(i[0]))
+        variable = Word(alphanums + "_" + "-")
+        # operator = oneOf("+ - * /")
+        string = QuotedString("\"", escChar="\\") ^\
+            QuotedString("\'", escChar="\\")
+
+        # larger blocks
+        function_call = Forward()
+        function_input = integer ^ variable ^ string ^ Group(function_call)
+        function_call << keyword + Suppress("(") + ZeroOrMore(
+            function_input + Suppress(Optional(","))) + Suppress(")")
+        function_calls = indentedBlock(function_call, indentationStack)
+
+        chunk = Forward()
+        chunk_starter = oneOf(blocks.startChunkBlocks) +\
+            Optional(function_input) + Suppress(":")
+        indented_chunk = indentedBlock(function_call ^ chunk, indentationStack)
+        chunk << chunk_starter + indented_chunk
+
+        hat_block = oneOf(blocks.hatBlocks) + Optional(function_input) +\
+            Suppress(":")
+        hat_chunk = hat_block + (function_calls ^ OneOrMore(chunk))
+
+        # largest blocks
+        script = OneOrMore(Group(hat_chunk)) + stringEnd
+
+        # make the string parsing function available throughout the class
+        self.parse = script.parseString
 
     def transform(self):
-        print "in transform"
-        indentationStack = [1]
-        functions = Forward()
-        period = Suppress(Literal("."))
-        #These are different types of inputs
-        integer = Word(nums).setParseAction(lambda t: int(t[0]))
-        variable = Combine(OneOrMore(Word(alphanums) + Optional(oneOf("_ -"))))
-        string = QuotedString('"', escChar='\\')
-        func = Group( Group(OneOrMore(Word(alphas)))+ Suppress("("")")).addParseAction()#this one is only for a function inside a nother function
-
-        #this part parses input when it is a expressions following order of operations
-        #negpos = oneOf('+ -') #this doesn't work currently in our xml_creator
-        multop = oneOf('* /')
-        plusop = oneOf('+ -')
-
-        exprinputs = Group(functions("func") | integer("integer") | variable("variable"))
-        expression =  operatorPrecedence( exprinputs, #Optional(Suppress(Literal("("))) +
-    [("!", 1, opAssoc.LEFT),
-     ("^", 2, opAssoc.RIGHT),
-     ("%", 2, opAssoc.LEFT),
-     #(signop, 1, opAssoc.RIGHT),
-     (multop, 2, opAssoc.LEFT),
-     (plusop, 2, opAssoc.LEFT),]
-    )#.addParseAction(lambda t: printx(t))#+ Optional(Suppress(Literal(")")))
-
-        #this is combination of all of the inputs
-        operand = Group(func("func") ^ integer("integer") ^ variable("variable") ^ string("string") ^ expression("expression"))
-        #this makes it so you can put multiple inputs in a function
-        regInput = Suppress(Literal("(")) + Group(Optional(operand + ZeroOrMore((Suppress(Literal(",")) | Suppress("to") | Suppress("for"))+ operand))) + Suppress(Literal(")"))
-
-        """
-        Currently if statements do not work
-        What it should parse:
-        if input >= input:
-            function
-            function
-        function
-        When run gives:
-        pyparsing.ParseException:  (at char 37), (line:1, col:38)
-        """
-        ifstatement = Suppress(Literal("if")) + operand("op1") + oneOf("<= < >= > =")("relation") +  operand("op2")
-        ifgroup = Group(Group(ifstatement) + indentedBlock(functions("functions"), [1]))
+        return self.parse(self.etch_code_string)
 
 
-        comments = Suppress(Optional(Literal("#") + restOfLine))
-
-        """
-        startCode is a the beginning of each script block
-        functions is a broad definition that uses regInput to recognize functions.
-        allFunctions is recognizes if statments and functions with comments after them
-        """
-        startCode = Group(Suppress(Optional(oneOf("when When")))+Group(OneOrMore(Word(alphas))) + Suppress(Literal(":")))#startCode = Group(CaselessKeyword("E") ^ CaselessKeyword("events") + period + Suppress(Optional(CaselessLiteral("when")))+Group(CaselessLiteral("flag") + CaselessLiteral("clicked")) + Suppress(Literal(":")))
-        functions = Group( Group(OneOrMore(Word(alphas)))("child") +regInput("reginput"))("function")#all functions must be on new line
-        allfunctions = OneOrMore(ifgroup + comments ^ functions("function")+ comments)
-        operand.setParseAction(
-        lambda origString,loc,tokens:
-            ( tokens[0], lineno(loc,origString), col(loc,origString) )
-        )
-        """
-        each script block includes when to start and then functions
-        fullCode is a collection of scriptblocks
-        """
-        scriptBlock = Group(startCode("startcode") + comments + allfunctions("functions"))
-        fullCode = OneOrMore(scriptBlock("scriptblock")) + StringEnd() #This raises a error if it doesn't finish parsing the string
-
-        print "about to return"
-        return fullCode.parseString(self.string)
+if __name__ == "__main__":
+    string = """
+flag clicked:
+    say("hi")
+flag clicked:
+    if 'fo':
+        say("second parallel hat block!")"""
+    print(toList(string).transform())
