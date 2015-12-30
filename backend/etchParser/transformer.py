a77b8845e10d5e97ebabca210ca1577d2fa1e2cb
Daniel F <danielf@openmailbox.org>
2015-12-30 15:07:45 -0500

    partial modfication to parse with parseAction

--- backend/etchParser/transformer.py
+++ backend/etchParser/transformer.py
@@ -1,45 +1,59 @@
-from pyparsing import *
+from pyparsing import *  # NOQA
 import blocks
 
-
 class toList:
-    def __init__(self, etch_code_string):
+    INPUT = "<l>%s</l>"
+    VARIABLE = "<block var=\"%s\"/>"
+    OPERATOR = "<block s=\"%s\">%s</block>"
+
+    def __init__(self, etch_code_string, variables):
         self.etch_code_string = etch_code_string  # the string we were passed
 
-        indentationStack = [1]  # this is used in all the indentedBlock's
+        # functions that transform with setParseAction
+        def parse_input(string, pos, tokens):
+            return self.INPUT % (tokens[0])
 
+        def parse_variable(string, pos, tokens):
+            return self.VARIABLE % (tokens[0])
+
+        # def parse_operator(string, pos, tokens, so_far=""):
+        #     if len(token) == 1:
+        #         return so_far % token
+        #     else:
+        #         return OPERATORparse_operator(string, pos, tokens[0], so_far=so_far) +\
+
+        indentationStack = [1]  # this is used in all the indentedBlock's
         # basic building blocks
         keyword = Word(alphanums)("keyword")
-        integer = Word(nums).setParseAction(lambda i: int(i[0]))("integer")
-        variable = Word(alphanums + "_" + "-")("variable")
-        # operator = oneOf("+ - * /") #TODO: Add operator support
-        string = QuotedString("\"", escChar="\\") ^\
-            QuotedString("\'", escChar="\\")("string")
+        integer = Word(nums).setParseAction(parse_input)
+        variable = Word(alphanums + "_" + "-")("variable").\
+            setParseAction(parse_variable)
+        string = (QuotedString("\"", escChar="\\") ^
+                  QuotedString("\'", escChar="\\")("string")).\
+            setParseAction(parse_input)
 
         # larger blocks
 
         function_call = Forward()
         # Forward is placeholder, we define it later
         # as input to a function there can be int, str, var, function call
-        function_input = integer ^ variable ^ string ^ Group(function_call) #these are the inputs
+        function_input = integer ^ variable ^ string ^ Group(function_call)
         multop = oneOf('* /')
         plusop = oneOf('+ -')
-        input = operatorPrecedence(function_input,
+        an_input = infixNotation(function_input, [
+            ("!", 1, opAssoc.LEFT),
+            ("^", 2, opAssoc.RIGHT),
+            ("%", 2, opAssoc.LEFT),
 
-    [("!", 1, opAssoc.LEFT),
-     ("^", 2, opAssoc.RIGHT),
-     ("%", 2, opAssoc.LEFT),
-
-
-     (multop, 2, opAssoc.LEFT),
-     (plusop, 2, opAssoc.LEFT),
-    ("and", 2, opAssoc.LEFT) ]
-    )
+            (multop, 2, opAssoc.LEFT),
+            (plusop, 2, opAssoc.LEFT)
+            ("and", 2, opAssoc.LEFT)
+        ]).setParseAction(parse_operator)
 
         # no we define funcion call, this had to be done after input
         # because we use input in the deffinition
         function_call << keyword + Suppress("(") + ZeroOrMore(
-            input + Suppress(Optional(","))) + Suppress(")")
+            an_input + Suppress(Optional(","))) + Suppress(")")
         # an indented block of ONLY function calls, chunk below covers if there
         # is additional indented stuff in the block
         function_calls = indentedBlock(function_call,
@@ -49,19 +63,20 @@ class toList:
         # statement. we define it later so a chunk is able to contain a chunk
         chunk = Forward()  # Forward is a placeholder
         chunk_starter = oneOf(blocks.startChunkBlocks) +\
-            Optional(input) + Suppress(":")  # a line like `if foo:`
+            Optional(an_input) + Suppress(":")  # a line like `if foo:`
         # indented_chunk is what comes after `if foo:`
         indented_chunk = indentedBlock(function_call ^ chunk, indentationStack)
         # here we define chunk that we initialized with Forward above
         chunk << chunk_starter + indented_chunk
 
-        hat_block = oneOf(blocks.hatBlocks) + Optional(input) +\
+        hat_block = oneOf(blocks.hatBlocks) + Optional(an_input) +\
             Suppress(":")  # a line like `flag clicked:` or `key pressed 'a':`
         # `flag clicked:` and the indented text after it. function calls is for
         # if there is no additional indent in the text indented after it
-        hat_chunk = hat_block("hat") + (function_calls ^ Group(OneOrMore(chunk)))("hat")
+        hat_chunk = hat_block("hat") +\
+            (function_calls ^ Group(OneOrMore(chunk)))("hat")
 
-        script = OneOrMore(Group(hat_chunk))("st") + stringEnd  # a full project
+        script = OneOrMore(Group(hat_chunk))("st") + stringEnd  # full project
 
         # just for ease-of-reading rename what we will parse against parser
         parser = script
@@ -77,11 +92,12 @@ class toList:
 if __name__ == "__main__":
     string = """
 flag clicked: # a comment
-    say("hi"+122 and 1) # another comment
+    say("hi"+ 122) # another comment
+    say(var_1)
     # full line comment
 flag clicked:
-    if 'fo': # yet another comment
+    if 'fo' and 1: # yet another comment
         # full line
         if 'fa':
-            say("second parallel hat block!")"""
-    print(toList(string).transform().asXML())
+            say(3 * 2 + 1)"""
+    print(toList(string, ["var_1"]).transform())
